60bf9f2f0074cffdba04aa800b7cde79d1ea5609
lhx <514401746@qq.com>
2019-02-21 11:00:38 +0800

    20190221 test

---
+++ python/0221test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+'''
+a = 100**2;
+if a >= 200:
+    print(a);
+else:
+    print(-a);
+
+print(r'\\waa...a');
+
+'''
+'''
+a='as';
+if a!='as':
+    print('a');
+else:
+    print('b');
+'''
+
+a = None;
+if a is None:
+    print ('a is none\n');
+print (type(a));
+
+print ('a'+'b\n');
+
+print(round(2.655,2));
